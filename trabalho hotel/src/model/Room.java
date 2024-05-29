d94797007504e3ddbfb7e143305d98d7afccb816
Km2502 <kevenmendes2502@gmail.com>
2024-05-29 20:42:12 -0300

    Add files via upload

---
+++ trabalho hotel/src/model/Room.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+public class Room {
+    private int roomNumber;
+    private boolean isAvailable;
+
+    public Room(int roomNumber, boolean isAvailable) {
+        this.roomNumber = roomNumber;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Quarto " + roomNumber + (isAvailable ? " - Disponível" : " - Ocupado");
+    }
+}
+
+
